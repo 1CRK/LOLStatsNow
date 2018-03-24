40c435aac97edc04c4e32b3f76dc9bbe84c95b10
Chris <christopher.r.kucera@gmail.com>
2018-03-23 21:17:33 -0400

    :)

--- LOLStatsNow/ViewController.swift
+++ LOLStatsNow/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainAction: UIButton!
+    @IBOutlet weak var secondaryAction: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func runicPath(_sender: UIButton){
+        
+    }
+    @IBAction func statPath(_sender: UIButton){
+        
+    }
 }
 
