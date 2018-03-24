197ab7d1ef4340cf87ff8e7ee1eef4c4e1e08640
Chris <christopher.r.kucera@gmail.com>
2018-03-23 20:12:08 -0400

    Initial Commit

---
+++ LOLStatsNow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LOLStatsNow
+//
+//  Created by Kucera, Christopher on 3/23/18.
+//  Copyright © 2018 Kucera, Christopher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
